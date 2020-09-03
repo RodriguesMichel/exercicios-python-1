3b79106144148b9c6334d99b67ea8941e2c4e8c0
Michel Rodrigues <michel.rdrigues@hotmail.com>
2020-09-03 17:13:27 -0300

    ex001 - ex004

---
+++ ex002.py
@@ -0,0 +1,6 @@
+# Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+
+nome = input('Digite o seu nome: ')
+print('Seja Bem-vindo ' + nome + ' prazer em conhece-lo.')
+
+print('Seja muito bem-vindo {}, é um enorme prazer em te conhecer.'.format(nome))
